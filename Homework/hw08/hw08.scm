4e90a8f9f329f1a41cde3d28c68f1012a9a2d95c
Daniel <zyddawn1995@gmail.com>
2018-03-21 18:15:06 -0700

    Got stuck on Q5

--- Homework/hw08/hw08.scm
+++ Homework/hw08/hw08.scm
@@ -1,26 +1,39 @@
 (define (cddr s)
+  ;'YOUR-CODE-HERE
   (cdr (cdr s)))
 
 (define (cadr s)
+  ;'YOUR-CODE-HERE
   (car (cdr s))
 )
 
 (define (caddr s)
+  ;'YOUR-CODE-HERE
   (car (cdr (cdr s)))
 )
 
 (define (sign x)
-  'YOUR-CODE-HERE
+  ;'YOUR-CODE-HERE
+  (cond ((< x 0) -1) 
+        ((= x 0) 0) 
+        (else 1)) 
 )
 
 (define (square x) (* x x))
 
 (define (pow b n)
-  'YOUR-CODE-HERE
+  ;'YOUR-CODE-HERE
+  (cond ((= n 0) 1)  ;don't forget base case
+        ((even? n) (square (pow b (/ n 2))))
+        (else (* b (square (pow b (/ (- n 1) 2))))))
 )
 
 (define (ordered? s)
-  'YOUR-CODE-HERE
+  ;'YOUR-CODE-HERE
+  (cond ((null? s) #t)          ;hard to debug...
+        ((null? (cdr s)) #t)
+        ((<= (car s) (cadr s)) (ordered? (cdr s)))
+        (else #f))
 )
 
 (define (nodots s)
--- Lab/lab09/tests/remove.py
+++ Lab/lab09/tests/remove.py
@@ -1,45 +1,49 @@
 test = {
-  'name': 'nodots',
+  'name': 'remove',
   'points': 1,
   'suites': [
     {
       'cases': [
         {
           'code': r"""
-          scm> (nodots '(1 . 2))
-          (1 2)
+          scm> (remove 3 nil)
+          f9ebafa0bfa75e2a858c464aa39a573d
+          # locked
           """,
           'hidden': False,
-          'locked': False
+          'locked': True
         },
         {
           'code': r"""
-          scm> (nodots '(1 2 . 3))
-          (1 2 3)
+          scm> (remove 2 '(1 3 2))
+          4274c7da2de8112f582f10ef20b2d371
+          # locked
           """,
           'hidden': False,
-          'locked': False
+          'locked': True
         },
         {
           'code': r"""
-          scm> (nodots '((1 . 2) 3))
-          ((1 2) 3)
+          scm> (remove 1 '(1 3 2))
+          a69c1998a934bceac4c7b234f084e250
+          # locked
           """,
           'hidden': False,
-          'locked': False
+          'locked': True
         },
         {
           'code': r"""
-          scm> (nodots '(1 (2 3 . 4) . 3))
-          (1 (2 3 4) 3)
+          scm> (remove 42 '(1 3 2))
+          09c9e5e54b16e44b6676cd663e135ab4
+          # locked
           """,
           'hidden': False,
-          'locked': False
+          'locked': True
         },
         {
           'code': r"""
-          scm> (nodots '(1 . ((2 3 . 4) . 3)))
-          99664260ecd386c62f28106482facddd
+          scm> (remove 3 '(1 3 3 7))
+          14857fc721cca1cd17cf1d9e3404fd2a
           # locked
           """,
           'hidden': False,
@@ -48,7 +52,8 @@ test = {
       ],
       'scored': True,
       'setup': r"""
-      scm> (load 'hw08)
+      scm> (load 'lab09)
+      scm> (load 'lab09_extra)
       """,
       'teardown': '',
       'type': 'scheme'
