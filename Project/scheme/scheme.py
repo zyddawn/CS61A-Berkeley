1f9b55576a304d0a46da33c250a8de38dabefacb
Daniel <zyddawn1995@gmail.com>
2018-04-07 01:33:18 -0700

    Finish q6

--- Project/scheme/scheme.py
+++ Project/scheme/scheme.py
@@ -213,7 +213,14 @@ def do_define_form(expressions, env):
     if scheme_symbolp(target):
         check_form(expressions, 2, 2)
         # BEGIN PROBLEM 6
-        "*** YOUR CODE HERE ***"
+        # symbol = expressions.second           
+        # if symbol.second is nil:            # ugly code!!! any method to optimize it???
+        #    symbol = symbol.first
+        symbol = eval_all(expressions.second, env)
+        # LATENT BUG & INCOMPLETE (not sure about the usage of "eval_all")
+        value = scheme_eval(symbol, env)
+        env.define(target, value)
+        return target
         # END PROBLEM 6
     elif isinstance(target, Pair) and scheme_symbolp(target.first):
         # BEGIN PROBLEM 10
