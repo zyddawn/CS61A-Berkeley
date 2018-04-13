8a71522d7f229a862466000c87431b096a321321
Daniel <zyddawn1995@gmail.com>
2018-04-12 19:46:42 -0700

    Finish accumulate and accumulate-tail

--- Homework/hw10/hw10.scm
+++ Homework/hw10/hw10.scm
@@ -1,12 +1,17 @@
 (define (accumulate combiner start n term)
   (if (= n 0)
       start
-      'YOUR-CODE-HERE
+      ;'YOUR-CODE-HERE
+      (combiner (term n) (accumulate combiner start (- n 1) term))
   )
 )
 
 (define (accumulate-tail combiner start n term)
-  'YOUR-CODE-HERE
+  ;'YOUR-CODE-HERE
+  (if (= n 0)
+      start
+      (accumulate-tail combiner (combiner start (term n)) (- n 1) term)
+  )
 )
 
 (define-macro (list-of expr for var in seq if filter-fn)
