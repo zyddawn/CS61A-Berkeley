547045ab2223f15b8ed06403fce62a1cab26b505
Daniel <zyddawn1995@gmail.com>
2018-04-18 23:08:04 -0700

    Finish 50% of q3 has-cycle

--- Homework/hw11/hw11.scm
+++ Homework/hw11/hw11.scm
@@ -11,12 +11,25 @@
   (cond ((null? s) nil)
   		(else (cons-stream (* 2 (car s)) (scale-stream (cdr-stream s) k)))
   )
-
 )
 
 (define (has-cycle s)
-  'YOUR-CODE-HERE
+  ;'YOUR-CODE-HERE
+  (define (is-equal s sub_s step1 step2)
+    (cond ((null? sub_s) #f)
+          ((eq? s sub_s) 
+          	(if (= step1 step2)
+          		(is-equal s (cdr-stream sub_s) 0 (+ step2 1))
+          		#t
+          	)
+          )
+          (else (is-equal (cdr-stream s) sub_s (+ 1 step1) step2))
+    )
+  )
+  (is-equal s (cdr-stream s) 0 1)
 )
+
+
 (define (has-cycle-constant s)
   'YOUR-CODE-HERE
 )
