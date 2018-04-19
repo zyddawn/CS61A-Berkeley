d12cb6c8da41915598be6b971415879505333aa3
Daniel <zyddawn1995@gmail.com>
2018-04-18 18:53:08 -0700

    Finish q4 (all required questions done)

--- Lab/lab12/lab12.sql
+++ Lab/lab12/lab12.sql
@@ -3,15 +3,19 @@
 
 -- Q2
 CREATE TABLE obedience AS
-  SELECT seven, denero FROM students;
+  SELECT seven, denero 
+  FROM students;
 
 -- Q3
 CREATE TABLE smallest_int AS
-  SELECT time, smallest FROM students
-  		WHERE smallest > 15
-  		ORDER BY smallest ASC
-  		LIMIT 20;
+  SELECT time, smallest 
+  FROM students
+  WHERE smallest > 15
+  ORDER BY smallest ASC
+  LIMIT 20;
 
 -- Q4
 CREATE TABLE matchmaker AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT s1.pet, s1.song, s1.color, s2.color
+  FROM students AS s1, students AS s2
+  WHERE s1.time < s2.time AND s1.pet=s2.pet AND s1.song=s2.song; 
