a792c7ff69b78c09f38b164f3fdfb97aad591443
Daniel <zyddawn1995@gmail.com>
2018-04-25 01:03:08 -0700

    Finish q1

--- Lab/lab13/lab13.scm
+++ Lab/lab13/lab13.scm
@@ -1,8 +1,25 @@
 ; Q1
 (define (compose-all funcs)
-  'YOUR-CODE-HERE
-  nil
+  ;'YOUR-CODE-HERE
+  (define (my_func value)
+     (define (helper rest_func cur_func value)
+     	(if (null? rest_func)
+     		(if (null? cur_func)
+     			value
+     			(cur_func value)
+     		)
+     		(helper (cdr rest_func) (car rest_func) (cur_func value))
+     	)
+     )
+     (if (null? funcs)
+     	 (helper funcs funcs value)
+     	 (helper (cdr funcs) (car funcs) value)
+     )
+  )
+  my_func
 )
+
+
 
 ; Q2
 (define (tail-replicate x n)
